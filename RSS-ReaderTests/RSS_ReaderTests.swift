4c8c25d9e22cd5a1ab2c634dfe0c1272efd4db60
vitket <skolkojvastut@gmail.com>
2019-01-22 20:48:33 +0200

    update Unit Tests

--- RSS-ReaderTests/RSS_ReaderTests.swift
+++ RSS-ReaderTests/RSS_ReaderTests.swift
@@ -15,24 +15,13 @@ class RSS_ReaderTests: XCTestCase {
     let core = CoreRSS()
     
     func testArrayOfItems() {
-        
         let expectation = XCTestExpectation(description: "Parsing RSS test")
-        
-        let feedURL = URL(string: "https://www.wired.com/feed/rss")!
-        let parser = FeedParser(URL: feedURL) // or FeedParser(data: data) or FeedParser(xmlStream: stream)
-        // Parse asynchronously, not to block the UI.
-        parser.parseAsync(queue: DispatchQueue.global(qos: .userInitiated)) { (result) in
-            // Do your thing, then back to the Main thread
-            let feed = result.rssFeed?.items
-            let error = result.error
-            XCTAssertNil(error)
-            XCTAssertNotNil(feed)
-            
+        core.getData { (error) in
+            XCTAssertTrue(self.core.feed.count > 0)
             // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
             expectation.fulfill()
-            
         }
         wait(for: [expectation], timeout: 10.0)
     }
-
+    
 }
